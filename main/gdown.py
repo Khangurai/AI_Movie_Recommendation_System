aa36de70583341bb7b5696d26877e3693b4c7885
Ray_PC <glorykai7@gmail.com>
2024-08-04 20:59:46 +0630

    update file

---
+++ main/gdown.py
@@ -0,0 +1,19 @@
+import gdown
+import streamlit as st
+import pickle
+
+# Google Drive shareable link
+url = 'https://drive.google.com/file/d/1cC1AP0qXIQmOQ8iNiGj9DTE0WFXhw1E9/view?usp=drive_link'
+
+# Local file name where the file will be saved
+output = 'similarity_TfidfVectorizer.pkl'
+
+# Download the file
+gdown.download(url, output, quiet=False)
+
+# Load the pickle file
+with open(output, 'rb') as file:
+    data = pickle.load(file)
+
+# Now you can use 'data' in your app
+st.write(data)
